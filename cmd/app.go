30ba5ef4740610ae649b94eef783ef6a9ccd3e21
Raphael 'kena' Poss <knz@cockroachlabs.com>
2019-06-14 14:12:32 +0200

    Add an intro and improve the shutdown.

--- cmd/app.go
+++ cmd/app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"time"
 
 	"github.com/knz/shakespeare/cmd/stop"
 )
@@ -13,6 +15,9 @@ type app struct {
 	au *audition
 
 	// minTime and maxTime are used to compute the x range of plots.
+	// They are updated by the collector. Either can become negative if
+	// the observed system has a clock running in the past relative to the
+	// conductor.
 	minTime float64
 	maxTime float64
 }
@@ -28,11 +33,29 @@ func newApp(cfg *config) *app {
 }
 
 // expandTimeRange should be called for each processed event time stamp.
-func (a *app) expandTimeRange(instant float64) {
-	if instant > a.maxTime {
-		a.maxTime = instant
+func (ap *app) expandTimeRange(instant float64) {
+	if instant > ap.maxTime {
+		ap.maxTime = instant
 	}
-	if instant < a.minTime {
-		a.minTime = instant
+	if instant < ap.minTime {
+		ap.minTime = instant
 	}
+}
+
+func (ap *app) report(format string, args ...interface{}) {
+	if ap.cfg.quiet {
+		return
+	}
+	fmt.Printf(format, args...)
+	fmt.Println()
+}
+
+func (ap *app) intro() {
+	playedRoles := make(map[string]struct{})
+	for _, a := range ap.cfg.actors {
+		playedRoles[a.role.name] = struct{}{}
+	}
+	ap.report("welcome a cast of %d actors, playing %d roles",
+		len(ap.cfg.actors), len(playedRoles))
+	ap.report("the play is starting; expected duration: %s", ap.cfg.tempo*time.Duration(len(ap.cfg.play)))
 }

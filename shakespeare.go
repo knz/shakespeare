d6d8129dccc1896eb92dc9adb1ea68c122c20bfc
Raphael 'kena' Poss <knz@thaumogen.net>
2019-06-19 11:29:03 +0200

    Print error details/hints at end.

--- shakespeare.go
+++ shakespeare.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/cockroachdb/errors"
 	"github.com/knz/shakespeare/pkg/cmd"
 )
 
 func main() {
 	if err := cmd.Run(); err != nil {
 		fmt.Println(err)
+		if d := errors.FlattenDetails(err); d != "" {
+			fmt.Printf("--\n%s\n", d)
+		}
+		if d := errors.FlattenHints(err); d != "" {
+			fmt.Printf("HINT: %s\n", d)
+		}
 		os.Exit(1)
 	}
 }

01ebff2b5706bd44900d0aaf7e1ff30d6be5a91f
Raphael 'kena' Poss <knz@cockroachlabs.com>
2019-06-26 09:00:22 +0200

    Add some functions.

--- pkg/cmd/functions_test.go
+++ pkg/cmd/functions_test.go
@@ -16,7 +16,22 @@ func TestFunctions(t *testing.T) {
 		res  interface{}
 	}{
 		{x: 120., y: 100., expr: "ndiff(x,y)", res: .20},
+		{x: nil, y: 100., expr: "ndiff(x,y)", res: nil},
+		{x: 120., y: nil, expr: "ndiff(x,y)", res: nil},
+		{x: nil, y: nil, expr: "ndiff(x,y)", res: nil},
 		{x: -120., expr: "abs(x)", res: 120.},
+		{x: nil, expr: "abs(x)", res: nil},
+		{x: 1.3, expr: "ceil(x)", res: 2.0},
+		{x: nil, expr: "ceil(x)", res: nil},
+		{x: 1.3, expr: "floor(x)", res: 1.0},
+		{x: nil, expr: "floor(x)", res: nil},
+		{x: 1.3, expr: "round(x)", res: 1.0},
+		{x: 1.6, expr: "round(x)", res: 2.0},
+		{x: nil, expr: "round(x)", res: nil},
+		{x: 1.0, expr: "log(x)", res: 0.0},
+		{x: nil, expr: "log(x)", res: nil},
+		{x: 4.0, expr: "sqrt(x)", res: 2.0},
+		{x: nil, expr: "sqrt(x)", res: nil},
 		{x: []interface{}{2., 3., 1.}, expr: "count(x)", res: 3.},
 		{x: []interface{}{2., 3., 1.}, expr: "first(x)", res: 2.},
 		{x: []interface{}{2., 3., 1.}, expr: "last(x)", res: 1.},
@@ -24,6 +39,8 @@ func TestFunctions(t *testing.T) {
 		{x: []interface{}{1., 2., 3., 4.}, expr: "avg(x)", res: 2.5},
 		{x: []interface{}{1., 2., 3., nil}, expr: "avg(x)", res: 2.},
 		{x: []interface{}{1., 1., 2., 3.}, expr: "med(x)", res: 1.5},
+		{x: []interface{}{1., 2., 3., 4.}, expr: "min(x)", res: 1.0},
+		{x: []interface{}{1., 2., 3., 4.}, expr: "max(x)", res: 4.0},
 		{x: []interface{}{}, expr: "med(x)", res: nil},
 		{x: []interface{}{}, expr: "count(x)", res: 0.},
 		{x: []interface{}{}, expr: "first(x)", res: nil},
@@ -31,6 +48,8 @@ func TestFunctions(t *testing.T) {
 		{x: []interface{}{}, expr: "sum(x)", res: nil},
 		{x: []interface{}{}, expr: "avg(x)", res: nil},
 		{x: []interface{}{}, expr: "med(x)", res: nil},
+		{x: []interface{}{}, expr: "min(x)", res: nil},
+		{x: []interface{}{}, expr: "max(x)", res: nil},
 	}
 
 	for _, test := range testData {

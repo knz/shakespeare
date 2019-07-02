1e4a74bef95866c5b8eb04ea02218e332dca593d
Raphael 'kena' Poss <knz@thaumogen.net>
2019-07-02 10:09:39 +0200

    Implement preliminary behavior tests

---
+++ pkg/cmd/run_test.go
@@ -0,0 +1,105 @@
+package cmd
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/datadriven"
+	"github.com/knz/shakespeare/pkg/crdb/log"
+	"github.com/knz/shakespeare/pkg/crdb/timeutil"
+)
+
+var tmpDir = func() string {
+	if d, ok := os.LookupEnv("TMPDIR"); ok {
+		return d
+	}
+	return "/tmp"
+}
+
+func TestRun(t *testing.T) {
+	includePath := []string{filepath.Join("testdata", "include"), ""}
+	datadriven.Walk(t, filepath.Join("testdata", "run"), func(t *testing.T, path string) {
+		if strings.HasSuffix(path, "~") ||
+			strings.HasPrefix(path, ".#") ||
+			strings.HasSuffix(path, ".cfg") {
+			return
+		}
+		includePath[1] = filepath.Dir(path)
+		datadriven.RunTest(t, path, func(d *datadriven.TestData) string {
+			// Prepare a log scope for the test.
+			sc := log.Scope(t)
+			defer sc.Close(t)
+
+			// Parse+compile the configuration.
+			rd, err := newReaderFromString("<testdata>", d.Input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			rd.includePath = includePath
+			defer rd.close()
+			cfg := newConfig()
+
+			// Override the shell, to make error messages deterministic.
+			cfg.shellPath = "bash"
+
+			if err := cfg.parseCfg(context.TODO(), rd); err != nil {
+				t.Fatalf("parse error: %s\n", renderError(err))
+			}
+			if err := cfg.compileV2(); err != nil {
+				t.Fatalf("compile error: %s\n", renderError(err))
+			}
+
+			// Make the artifacts, etc. go to a separate directory for this test.
+			workDir, err := ioutil.TempDir("", "shakespeare-test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer func() {
+				if !t.Failed() {
+					if err := os.RemoveAll(workDir); err != nil {
+						t.Error(err)
+					}
+				} else {
+					t.Logf("artifacts and logs to be found in: %s", workDir)
+				}
+			}()
+			cfg.dataDir = workDir
+
+			// Disable plotting.
+			cfg.skipPlot = true
+			// Disable non-determistic narration output.
+			cfg.avoidTimeProgress = true
+
+			// Ensure the test terminates in a timely manner.
+			ctx, bye := context.WithDeadline(context.TODO(), timeutil.Now().Add(5*time.Second))
+			defer bye()
+
+			// Write the narration in memory.
+			var out bytes.Buffer
+			cfg.narration = &out
+
+			err = cfg.run(ctx)
+
+			if len(d.CmdArgs) > 0 && d.CmdArgs[0].Key == "error" {
+				if err == nil {
+					t.Error("expected error, got success")
+				} else {
+					fmt.Fprintf(&out, "run error: %s\n", renderError(err))
+				}
+			} else {
+				if err != nil {
+					t.Errorf("expected success, got error: %+v", err)
+				}
+			}
+
+			return out.String()
+		})
+	})
+}

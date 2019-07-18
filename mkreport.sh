4cb8a9327f7d4ddb38b123bf580f44db4e57ccbd
Raphael 'kena' Poss <knz@thaumogen.net>
2019-07-18 18:08:13 +0200

    fixup! Auto-gen the HTML-in-Go string

---
+++ mkreport.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -euo pipefail
+(
+	echo "package cmd"
+	echo
+	echo "// Note: the following is automatically generated from report.html."
+	printf "const reportHTML = \`"
+	sed -e 's/^[ 	]*//g' < pkg/cmd/report.html | grep -v '^$'
+	printf "\`"
+) >pkg/cmd/report_html.go
